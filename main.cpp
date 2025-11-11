826d0500ba9e3eb6e0f42b8465d52f292af6ff10
ziadMohamed16006210 <mziad908070@gmail.com>
2025-11-11 09:55:23 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,30 @@
 using namespace std;
 
 // ==================== Person Class Implementation =========================
-
+public:
+       void display(){
+        cout<<name<<id<<endl;
+       };
+       void set Name(String n){
+        name=n
+       };
+       String get Name{
+        return name;
+       };
+       void set Id(int n){
+        return Id;
+       };
+       String get Id(){
+        return Id
+       };
+       person(){
+        name=" ";
+        id=0;
+       };
+       person(int i,String n){
+        name=n;
+        id=i;
+       }
 
 
 
@@ -18,7 +41,38 @@ using namespace std;
 
 
 // ==================== Student Class Implementation ====================
+Student class:public person{
+        private:
+            int yearLevel;
+            string major;
+        public:
+            Student(){
+                yearLevel=0;
+                major=" ";
 
+            };
+            Student(string name,int id,int yearLevel,string major){
+                yearLevel=yearLevel;
+                major=major;
+
+            };
+            void set YearLevel(int n){
+                yearLevel=n;
+            };
+            int get YearLevel(){
+                return yearLevel;
+            };
+            void set major(string m){
+                major=m;
+
+            };
+            int get major(){
+                return major;
+            };
+            void display(){
+                cout<<yearLevel<<major<<endl;
+            };
+       }
 
 
 
