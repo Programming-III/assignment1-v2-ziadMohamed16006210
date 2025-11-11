693197960d90535318bc7604bc7bd5e3e0e63957
ziadMohamed16006210 <mziad908070@gmail.com>
2025-11-11 09:52:08 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -5,6 +5,19 @@
 #include <string>
 #using namespace std;
 //#write student class here
+tudent class:public person{
+        private:
+            int yearLevel;
+            string major;
+        public:
+            Student(){};
+            Student(string name,int id,int yearLevel,string major){};
+            void set YearLevel(int n){};
+            int get YearLevel(){};
+            void set major(){};
+            int get major(){};
+            void display(){};
+       }
 
 
 
