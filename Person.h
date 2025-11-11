bd0dd9c77f99690117d11b8053f370ce9456bff9
ziadMohamed16006210 <mziad908070@gmail.com>
2025-11-11 09:51:48 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -7,6 +7,23 @@ using namespace std;
 //#write Person class here
 
 
+class person {
+    private:
+      String name;
+      int id;
+
+    public:
+       void display(){};
+       void set Name(String n){};
+       String get Name{};
+       void set Id(int n){};
+       String get Id(){};
+       person(){};
+       person(int i,String n){}
+           
+       
+}#endif;
+
 
 
 
